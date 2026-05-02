6204abf74f334533b68042bc8d6dccd1a1cde359
daniel-sd03 <dsodre2003@gmail.com>
2026-05-01 21:11:59 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Home Beauty
+
+Projeto em desenvolvimento 🚧
+
+Frontend em React para conectar clientes a profissionais de beleza.
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-500 text-white">
+    Helo Word
+    </div>
+  );
+}  
